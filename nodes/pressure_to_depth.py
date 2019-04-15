0b4062d7164b5f3acfee9d4153cca04eb70d9aa7
Sebastian <Sebastian@helzer.eu>
2019-04-15 14:10:48 -0600

    Dynamic reconfigure depth node

--- nodes/pressure_to_depth.py
+++ nodes/pressure_to_depth.py
@@ -10,9 +10,18 @@ import numpy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from std_msgs.msg import Float32
 
+from dynamic_reconfigure.server import Server
+from fake_imu.cfg import depthConfig
+
 def callback(data):
     global pressure
     pressure = data.data
+
+def reconfig_callback(config, level):
+    global pressureOffset, pressureScale
+    pressureOffset = config['sensor_offset']
+    pressureScale = config['sensor_scale']
+    return config
     
 pressure = 0
 z = 0.0   
@@ -23,7 +32,7 @@ rospy.init_node("pressure_to_depth")
 #We only care about the most recent measurement, i.e. queue_size=1
 sub = rospy.Subscriber('pressure', Float32, callback)
 pub = rospy.Publisher('depth', PoseWithCovarianceStamped, queue_size=1)
-
+srv = Server(depthConfig, reconfig_callback)  # define dynamic_reconfigure callback
 rate = rospy.Rate(30) # 30hz
 
 poseMsg = PoseWithCovarianceStamped()
@@ -44,7 +53,7 @@ while not rospy.is_shutdown():
     poseMsg.pose.pose.position.x = 0
     poseMsg.pose.pose.position.y = 0
     poseMsg.pose.pose.position.z = z
-    poseMsg.header.frame_id = 'odom'
+    poseMsg.header.frame_id = 'base_imu_link'
     poseMsg.header.stamp = rospy.get_rostime()
     poseMsg.header.seq = seq
     seq = seq + 1
